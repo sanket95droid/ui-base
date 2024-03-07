8da7c9bf70da10d63ed5d6cda1a0871969cbdb77
sanket95droid <sanketzambre1@gmail.com>
2024-03-07 17:33:30 +0530

    (*)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
+    <>
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
         <h1 className="text-gray-500 text-4xl font-bold tracking-tight sm:text-4xl">
@@ -17,5 +18,13 @@ export default function Home() {
         </div>
       </div>
     </MaxWidthWrapper>
+    <section className="border-t boreder-gray-200 bg-gray-50">
+      <MaxWidthWrapper className="py-20">
+        <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+          
+        </div>
+      </MaxWidthWrapper>
+    </section>
+    </>
   );
 }
